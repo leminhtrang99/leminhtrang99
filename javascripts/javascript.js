a4681e0ab2e0c0b265ca61981ca446589b92769c
letm <letm@lafayette.edu>
2019-07-06 20:08:25 +0700

    responsive menu

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,12 +1,12 @@
 
 function responsiveNavbar(){
-    var x = document.getElementById("myNavBar");
-    console.log(document.getElementById("myNavBar").className);
-    if (document.getElementById("myNavBar").className == "row navbar") {
+    var x = document.getElementById("navBar");
+    console.log(document.getElementById("navBar").className);
+    if (document.getElementById("navBar").className == "row navbar") {
         
         x.classList.add('responsive');
     } else {
-        document.getElementById("myNavBar").className = "row navbar";
+        document.getElementById("navBar").className = "row navbar";
     }
 }
 
