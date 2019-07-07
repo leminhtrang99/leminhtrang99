dbc21e0048e87499b1b35aaf61f162f1edc5da07
letm <letm@lafayette.edu>
2019-07-08 00:16:37 +0700

    responsive sidebar icon change

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,5 +1,15 @@
 
+
 function revealSidebar(){
     document.getElementById('sideBar').classList.toggle('is-reveal');
+    document.getElementById('sideBarHide').classList.toggle('is-hidden');
+    document.getElementById('sideBarReveal').classList.toggle('is-hidden');
 }
-document.getElementById("navbarIcon").addEventListener("click", revealSidebar);
+
+function hideSidebar(){
+    document.getElementById('sideBar').classList.toggle('is-reveal');
+    document.getElementById('sideBarHide').classList.toggle('is-hidden');
+    document.getElementById('sideBarReveal').classList.toggle('is-hidden');
+}
+document.getElementById("sideBarReveal").addEventListener("click", revealSidebar);
+document.getElementById("sideBarHide").addEventListener("click", hideSidebar);
