a665024d8ec7bc12dfb514b380338fbbee521b2f
letm <letm@lafayette.edu>
2019-09-21 23:04:59 -0400

    Satan is Tweeting

---
+++ javascripts/satan-is-tweeting/markov-chain.js
@@ -0,0 +1,26 @@
+var MarkovChain = require('markovchain');
+var fs = require('fs')
+var quotes='';
+
+var quotes = new MarkovChain(fs.readFileSync('satan-del.txt', 'utf8'));
+var wordList = fs.readFileSync('satan-del.txt', 'utf8');
+//console.log(wordList);
+
+var useUpperCase = function(wordList) {
+  var tmpList = Object.keys(wordList).filter(function(word) {
+    return word[0] >= 'A' && word[0] <= 'Z'
+  })
+  return tmpList[~~(Math.random()*tmpList.length)]
+}
+var stopAtTweetLength = function(sentence) {
+    return sentence.length >= 280;
+}
+var result = quotes.start(useUpperCase).end(stopAtTweetLength).process();
+
+document.getElementById('tweetButton').addEventListener('click',showTweet);
+function showTweet(){
+  document.getElementById('tweet').innerHTML = result;
+}
+console.log(result);
+
+
